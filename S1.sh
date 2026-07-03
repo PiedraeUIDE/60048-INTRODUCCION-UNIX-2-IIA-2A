f3de245dc2d1606fd928463619098ca97cb4868e
PiedraeUIDE <andpiedrasa@uide.edu.ec>
2026-07-03 01:37:12 +0000

    wip: S1 script

---
+++ S1.sh
@@ -0,0 +1,4 @@
+#Este comando muestra la versión del sistema operativo, la arquitectura del procesador y la fecha de su última actualización.
+uname -a 
+#Este comando permite ver qué intérprete de comandos está usando la terminal de la máquina virtual.
+echo $SHELL
