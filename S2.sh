998ab7907f5a8c006a5af258ae73b8e7f0611988
PiedraeUIDE <andpiedrasa@uide.edu.ec>
2026-07-10 00:57:37 +0000

    wip: S2

--- S2.sh
+++ S2.sh
@@ -1,6 +1,22 @@
 #
-ls 
+ssh bandit0@bandit.labs.overthewire.org -p 2220
 #
-cd 
+history
 #
-rm 
+clear
+#
+pwd
+#
+cd
+#
+la
+#
+ls --all
+#Archivos y drirectorios con "." son ocultos 
+#cd . sinbolisa el directoria actual y el 
+cd .
+#cd .. te muece a la carpeta de un nivel ariva o padre del directorio actual 
+cd ..
+#cd / se movilisa a la carpeta root de del sistema linux 
+#
+ls -a /workspaces/60048-INTRODUCCION-UNIX-2-IIA-2A/carpeta/carpeta_hija
